cb317e96a2e98c9bff2521a48a560b8ed34a3515
Fabillotic <fabillo552@gmail.com>
2021-05-09 17:05:13 +0200

    Added stackmapstuff

---
+++ stackmapstuff.py
@@ -0,0 +1,15 @@
+from bytecompiler import Attribute
+
+class VerificationType:
+    def __init__(self, tag, info):
+        self.tag = tag
+        self.info = info
+
+class Frame:
+    def __init__(self):
+        pass
+
+class StackMapTableAttribute(Attribute):
+    def __init__(self):
+        pass
+
